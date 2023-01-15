e4f00fb5af87eb8ac1643d8d4af4da793279cad6
Enno Schnackenberg <enno.schnackenberg@web.de>
2023-01-15 16:58:12 +0100

    Update inverter.v

--- src/inverter.v
+++ src/inverter.v
@@ -1,4 +1,4 @@
-// Copyright 2022 Enno Schnackenberg
+// Copyright 2023 Enno Schnackenberg
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
